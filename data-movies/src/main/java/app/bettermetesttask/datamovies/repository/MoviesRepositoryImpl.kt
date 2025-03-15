2628217bbf594eb90eb46d19c4db712fad8f5326
Viнt@rь <vihtarb@gmail.com>
2025-03-15 17:36:39 +0200

    Added comment for reviewer

--- data-movies/src/main/java/app/bettermetesttask/datamovies/repository/MoviesRepositoryImpl.kt
+++ data-movies/src/main/java/app/bettermetesttask/datamovies/repository/MoviesRepositoryImpl.kt
@@ -5,11 +5,18 @@ import app.bettermetesttask.datamovies.repository.stores.MoviesLocalStore
 import app.bettermetesttask.datamovies.repository.stores.MoviesMapper
 import app.bettermetesttask.datamovies.repository.stores.MoviesRestStore
 import app.bettermetesttask.domaincore.utils.Result
-import app.bettermetesttask.domaincore.utils.connectivity.ConnectivityManager
 import app.bettermetesttask.domainmovies.entries.Movie
 import app.bettermetesttask.domainmovies.repository.MoviesRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
+
+/*
+
+Можно много чего было сделать или переделать, я постарался реализовать задачи с как можно меньшим импактом кода(не считая реформата кода :)).
+Хотя простора для различия реализаций здесь валом.
+Конечно я мог затянуть в проект свои наработки по работе с Result, Flow, MVI, States, Locale Data Store/RemoteDataStore однако выбрал такой путь :)
+
+ */
 
 class MoviesRepositoryImpl @Inject constructor(
     private val localStore: MoviesLocalStore,
